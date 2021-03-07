e76f5a95a36be75ed38e64d66015579d7ed82d70
Tatyana <79045855+taccianka@users.noreply.github.com>
2021-03-07 15:56:05 +0300

    fix: added support of empty array

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ module.exports = function towelSort(matrix) {
     let count = 0;
     let mas = [];
 
-    for (i = 0; i < matrix.length; i++) {
-        if (i % 2 == 0) {
-            for (j = 0; j < matrix[0].length; j++) {
-                mas[count] = matrix[i][j];
-                count++;
-            }
-        } else if (i % 2 == 1) {
-            for (j = matrix[0].length-1; j >= 0; j--) {
-                mas[count] = matrix[i][j];
-                count++;
+    if (matrix && matrix.length > 0) {
+        for (i = 0; i < matrix.length; i++) {
+            if (i % 2 == 0) {
+                for (j = 0; j < matrix[0].length; j++) {
+                    mas[count] = matrix[i][j];
+                    count++;
+                }
+            } else if (i % 2 == 1) {
+                for (j = matrix[0].length - 1; j >= 0; j--) {
+                    mas[count] = matrix[i][j];
+                    count++;
+                }
             }
         }
     }
+    
     return mas;
 };
