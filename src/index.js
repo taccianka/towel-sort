3a75df9cc77e97f121ad26d6288cfabef49e5384
Tatyana <79045855+taccianka@users.noreply.github.com>
2021-03-07 15:58:28 +0300

    fix: fix troubles with last test

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ module.exports = function towelSort(matrix) {
     if (matrix && matrix.length > 0) {
         for (i = 0; i < matrix.length; i++) {
             if (i % 2 == 0) {
-                for (j = 0; j < matrix[0].length; j++) {
+                for (j = 0; j < matrix[i].length; j++) {
                     mas[count] = matrix[i][j];
                     count++;
                 }
             } else if (i % 2 == 1) {
-                for (j = matrix[0].length - 1; j >= 0; j--) {
+                for (j = matrix[i].length - 1; j >= 0; j--) {
                     mas[count] = matrix[i][j];
                     count++;
                 }
