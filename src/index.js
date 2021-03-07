f3fbfec8f5c175d57981c08bcb958866888e0a1d
Tatyana <79045855+taccianka@users.noreply.github.com>
2021-03-07 15:38:54 +0300

    feat: write function towelSort()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+module.exports = function towelSort(matrix) {
+    let i, j;
+    let count = 0;
+    let mas = [];
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    for (i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+            for (j = 0; j < matrix[0].length; j++) {
+                mas[count] = matrix[i][j];
+                count++;
+            }
+        } else if (i % 2 == 1) {
+            for (j = matrix[0].length-1; j >= 0; j--) {
+                mas[count] = matrix[i][j];
+                count++;
+            }
+        }
+    }
+    return mas;
+};
